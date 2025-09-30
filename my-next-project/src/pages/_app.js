779f49d6344c93f0a12024edc1d5a3419f67153e
Emilaine Figueiredo <emilaine-alves@hotmail.com>
2025-09-30 16:42:46 +0100

    padrões
    Definido no app.js e no global.css os padrões: background e fontes.

--- my-next-project/src/pages/_app.js
+++ my-next-project/src/pages/_app.js
@@ -13,7 +13,7 @@ const mont = Montserrat_Alternates({
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className={`${mont.className} min-h-screen bg-slate-50 flex flex-col`}>
+    <div className={`${mont.className} min-h-screen bg-gradient-to-b from-cyan-400 via-purple-400 to-pink-500 text-white flex flex-col`}>
       <Navbar />
       <main className="container mx-auto px-4 py-8 flex-grow">
         <Component {...pageProps} />
